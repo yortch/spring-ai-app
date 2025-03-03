2fc6e1725dcc5fb9ecd725a3a4b2f36d2ec9c6e1
Jorge Balderas <jorgerbf@gmail.com>
2025-03-03 18:01:34 -0500

    updates after recreating project

---
+++ src/test/java/com/example/springaiapp/SpringAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springaiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
